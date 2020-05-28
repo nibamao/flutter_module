68efbbb5272704f36d4e3b2cd117b094195873d9
13834594579@163.com <199206240014z>
2020-05-28 16:33:56 +0800

    123cccc

--- lib/module/view.dart
+++ lib/module/view.dart
@@ -5,5 +5,10 @@ import 'package:fluttermodule/module/state.dart';
 
 
 Widget buildView(moduleState state, Dispatch dispatch, ViewService viewService) {
-  return Container();
+  return Scaffold(
+    appBar: new AppBar(
+      title: new Text('Grid页面'),
+    ),
+    body: Text("Grid页面"),
+  );
 }
