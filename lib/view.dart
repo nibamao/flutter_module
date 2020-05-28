48ca3957b2cdb5677bd32d85c5278fdd514f59c9
13834594579@163.com <199206240014z>
2020-05-28 16:22:58 +0800

    123cccc

--- lib/view.dart
+++ lib/view.dart
@@ -1,8 +1,8 @@
 import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttermodule/state.dart';
 
-import 'action.dart';
-import 'state.dart';
+
 
 Widget buildView(moduleState state, Dispatch dispatch, ViewService viewService) {
   return Container();
